b35b943810c917ba3a5a01be1ef63422ddb1b820
TANUJA DESHMUKH <137862508+TANUJA909@users.noreply.github.com>
2023-07-04 13:18:55 +0530

    Hello World (STS)

--- practicals/HelloWorld.java
+++ practicals/HelloWorld.java
@@ -1,8 +1,12 @@
+package oops;
+
 //to print simple "hello world"
-class HelloWorld
-{
-public static void main(String[] args)
-{
-System.out.println("Hello World");
-}
+ class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+
+	}
+
 }
