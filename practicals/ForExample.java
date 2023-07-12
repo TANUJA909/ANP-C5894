30bb755a1ad0a3ce95f2c9f3d8edc5a8d1c12dc0
TANUJA DESHMUKH <137862508+TANUJA909@users.noreply.github.com>
2023-07-12 13:52:59 +0530

    Simple For Loop example

---
+++ practicals/ForExample.java
@@ -0,0 +1,12 @@
+package loops;
+//Simple For Loop example
+public class ForExample 
+{
+	public static void main(String[] args) 
+	{
+		for(int i=0;i<=10;i++)
+		{
+			System.out.println("Values of i is :"+ i);
+		}
+	}
+}
