950364d62e7dbd00acaf8c6bf7d43b264c5a4cda
TANUJA DESHMUKH <137862508+TANUJA909@users.noreply.github.com>
2023-07-12 14:11:30 +0530

    For Loop example
    A Java program that prints a right-angled triangle made of asterisks (*) based on user input:

---
+++ practicals/RightAngleTriangle.java
@@ -0,0 +1,23 @@
+package loops;
+//A Java program that prints a right-angled triangle made of asterisks (*) based on user input:
+import java.util.Scanner;
+public class RightAngleTriangle 
+{
+	public static void main(String[] args) 
+	{
+		//Scanner class object
+		Scanner s=new Scanner(System.in);
+		
+		System.out.print("Enter the number of rows for the triangle: ");
+        int rows = s.nextInt();
+        for (int i = 1; i <= rows; i++) 
+        {
+            for (int j = 1; j <= i; j++) 
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        s.close();
+	}
+}
